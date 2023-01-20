112c3f679579598e65385f95232f874aa3ef1f47
Jayden Nawotka <j.nawotka9@gmail.com>
2023-01-20 14:01:54 +1100

    added startup file and updated download files for updater

---
+++ startup.lua
@@ -0,0 +1,17 @@
+if not fs.exists("startup.lua") then
+    local startfile = fs.open("startup.lua","w")
+    local request = http.get("https://raw.githubusercontent.com/OnStageDevelopment/DropPulleyControl/main/startup.lua")
+    startfile.write(request.readAll())
+    startfile.close()
+    request.close()
+    os.reboot()
+end
+
+updatercode = http.get("https://raw.githubusercontent.com/OnStageDevelopment/CurtainControl/main/updater.lua")
+ingested = updatercode.readAll()
+updater_file = fs.open("updater.lua","w")
+updater_file.write(ingested)
+updater_file.close()
+updatercode.close()
+
+shell.run("updater.lua")
--- updater.lua
+++ updater.lua
@@ -4,13 +4,20 @@ if not fs.isDir("libs") then
     fs.makeDir("libs")
     print("Created libs Directory")
 end
-if not fs.exists("curr_pos.txt") then
-    local file = fs.open("curr_pos.txt","w")
+if not fs.exists("h_curr_pos.txt") then
+    local file = fs.open("h_curr_pos.txt","w")
+    io.write("Please enter current distance: ")
+    local distance = io.read()
+    file.write(distance)
+    file.close()
+    print("Created H_CurrentPosition File")
+elseif not fs.exists("v_curr_pos.txt") then
+    local file = fs.open("v_curr_pos.txt","w")
     io.write("Please enter current height: ")
     local height = io.read()
     file.write(height)
     file.close()
-    print("Created CurrentPosition File")
+    print("Created V_CurrentPosition File")
 end
 
 if not fs.exists("config.json") then
@@ -23,18 +30,18 @@ if not fs.exists("config.json") then
 end
 
 local jsonfile = fs.open("libs/json.lua","w")
-local request = http.get("https://raw.githubusercontent.com/OnStageDevelopment/DropPulleyControl/main/node/libs/json.lua")
+local request = http.get("https://raw.githubusercontent.com/OnStageDevelopment/CurtainControl/main/node/libs/json.lua")
 jsonfile.write(request.readAll())
 jsonfile.close()
 request.close()
 print("Updated JSON File")
 
-local dropsfile = fs.open("drops.lua","w")
-local request = http.get("https://raw.githubusercontent.com/OnStageDevelopment/DropPulleyControl/main/node/drops.lua")
+local dropsfile = fs.open("curtain.lua","w")
+local request = http.get("https://raw.githubusercontent.com/OnStageDevelopment/CurtainControl/main/node/curtain.lua")
 dropsfile.write(request.readAll())
 dropsfile.close()
 request.close()
 print("Updated Drops File")
 
 
-shell.run("drops")
+shell.run("curtains")
